9751a6176314e3e88d7cb84c17bdea99c2873d42
ptya <petr.szab@gmail.com>
2018-06-10 14:44:29 +0200

    fixed eslint errors on helper.js

--- frontend/owcompare/src/helper.js
+++ frontend/owcompare/src/helper.js
@@ -3,14 +3,12 @@ import counters from './data/counters';
 export const getRandomPoints = () => {
   const points = { ...counters };
   Object.entries(counters).forEach(([key, value]) => {
-    Object.entries(value).forEach(([k, v]) => {
-      const num = Math.floor((Math.random() * 100) + 1);
+    Object.keys(value).forEach(k => {
+      const num = Math.floor(Math.random() * 100 + 1);
       points[key][k] = num;
     });
   });
   return points;
 };
 
-export const capitalize = (str) => (
-  str.charAt(0).toUpperCase() + str.substr(1)
-)
+export const capitalize = str => str.charAt(0).toUpperCase() + str.substr(1);
