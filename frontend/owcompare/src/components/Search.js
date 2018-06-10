de595276499859d5bd5f8ebce5f109bb40f71ea9
ptya <petr.szab@gmail.com>
2018-06-10 14:15:45 +0200

    fixed eslint errors on Search.js

--- frontend/owcompare/src/components/Search.js
+++ frontend/owcompare/src/components/Search.js
@@ -1,7 +1,33 @@
 import React, { Component, Fragment } from 'react';
+import PropTypes from 'prop-types';
 import { Scrollbars } from 'react-custom-scrollbars';
 
 class Search extends Component {
+  static getDerivedStateFromProps(nextProps, prevState) {
+    const { search, availableHeroes } = nextProps;
+    let { cursor, filteredHeroes, lastSearch } = prevState;
+    if (search !== lastSearch) {
+      cursor = 0;
+      lastSearch = search;
+      filteredHeroes = Object.keys(availableHeroes)
+        .sort()
+        .filter(hero => {
+          if (search === '') return '';
+          return (
+            availableHeroes[hero].name.toLowerCase().indexOf(search.toLowerCase()) !== -1 ||
+            availableHeroes[hero].id.toLowerCase().indexOf(search.toLowerCase()) !== -1
+          );
+        });
+      return {
+        ...prevState,
+        cursor,
+        filteredHeroes,
+        lastSearch,
+      };
+    }
+    return null;
+  }
+
   constructor(props) {
     super(props);
     this.searchRef = React.createRef();
@@ -10,39 +36,16 @@ class Search extends Component {
   }
 
   state = {
-    toHide: true,
-    filteredHeroes: [],
     cursor: 0,
+    filteredHeroes: [],
+    lastSearch: null,
+    toHide: true,
   };
 
-  static getDerivedStateFromProps(nextProps, prevState) {
-    const { search, availableHeroes } = nextProps
-    const filteredHeroes = Object.keys(availableHeroes).sort().filter(
-      (hero) => {
-        if (search === '') return '';
-        return availableHeroes[hero]
-                .name
-                .toLowerCase()
-                .indexOf(search.toLowerCase()) !== -1 || availableHeroes[hero]
-                .id
-                .toLowerCase()
-                .indexOf(search.toLowerCase()) !== -1;
-      }
-    );
-
-    return {
-      ...prevState,
-      filteredHeroes,
-    };
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (prevProps.search !== this.props.search) {
-      this.setState({ cursor: 0 })
-    }
+  componentDidUpdate() {
     // only try to focus if the element is present and the results
     // should not be hidden
-    const {toHide} = this.state;
+    const { toHide } = this.state;
     if (this.searchRef.current && !toHide) {
       this.searchRef.current.focus();
     }
@@ -53,13 +56,9 @@ class Search extends Component {
     if (active && results.container) {
       const activeBounding = active.getBoundingClientRect();
       const resultsBounding = results.container.getBoundingClientRect();
-      if (
-        Math.floor(activeBounding.bottom) > Math.floor(resultsBounding.bottom)
-      ) {
+      if (Math.floor(activeBounding.bottom) > Math.floor(resultsBounding.bottom)) {
         active.scrollIntoView(false);
-      } else if (
-        Math.ceil(activeBounding.top) < Math.ceil(resultsBounding.top)
-      ) {
+      } else if (Math.ceil(activeBounding.top) < Math.ceil(resultsBounding.top)) {
         active.scrollIntoView(true);
       }
     }
@@ -67,42 +66,43 @@ class Search extends Component {
 
   heroList = (hero, i) => {
     const { cursor } = this.state;
-    let listItemClass = ['search-list-item'];
-    if(cursor === i) {
+    const listItemClass = ['search-list-item'];
+    if (cursor === i) {
       listItemClass.push('active');
     }
     return (
-      <li
-        className={ listItemClass.join(' ') }
+      <button
+        className={listItemClass.join(' ')}
         key={i}
         ref={cursor === i ? this.activeRef : i}
         id={hero}
-        onMouseDown={this.handleClick.bind(this, hero)}
-        onMouseOver={this.handleHover.bind(this, i)}
+        onMouseDown={() => this.handleClick(hero)}
+        onMouseOver={() => this.handleHover(i)}
+        onFocus={() => this.handleHover(i)}
       >
         {this.props.availableHeroes[hero].name}
-      </li>
-    )
-  }
+      </button>
+    );
+  };
 
-  handleSubmit = (e) => {
+  handleSubmit = e => {
     e.preventDefault();
-    const {filteredHeroes, cursor} = this.state;
+    const { filteredHeroes, cursor } = this.state;
     if (filteredHeroes.length > 0) {
       this.props.updateSelected(filteredHeroes[cursor]);
     }
-  }
+  };
 
-  handleClick = (heroId) => {
+  handleClick = heroId => {
     this.props.updateSelected(heroId);
-  }
+  };
 
   handleFocus = () => {
     this.setState({ toHide: false });
-  }
+  };
 
   handleBlur = () => {
-    const {filteredHeroes} = this.state;
+    const { filteredHeroes } = this.state;
     // if intentionally clicked outside, hide the search results
     // else bring back the focus to the input field
     if (filteredHeroes.length > 0) {
@@ -110,29 +110,32 @@ class Search extends Component {
     } else {
       this.searchRef.current.focus();
     }
-  }
+  };
 
-  handleHover = (i) => {
-    this.setState( { cursor: i });
-  }
+  handleHover = i => {
+    this.setState({ cursor: i });
+  };
 
-  handleKeyDown = (e) => {
-    if (e.keyCode === 38) { // Arrow up
+  handleKeyDown = e => {
+    if (e.keyCode === 38) {
+      // Arrow up
       e.preventDefault();
-      const len = this.props.search.length * 2 // Opera sometimes sees a carriage return as 2 characters
+      const len = this.state.lastSearch.length * 2; // Opera sometimes sees a carriage return as 2 characters
       this.searchRef.current.setSelectionRange(len, len);
     }
-    const {cursor, filteredHeroes} = this.state;
+    const { cursor, filteredHeroes } = this.state;
     const listLength = filteredHeroes.length;
-    if (e.keyCode === 40 && cursor < listLength - 1 ) { // Arrow down
+    if (e.keyCode === 40 && cursor < listLength - 1) {
+      // Arrow down
       this.setState({ cursor: cursor + 1 });
-    } else if (e.keyCode === 38 && cursor > 0) { // Arrow up
+    } else if (e.keyCode === 38 && cursor > 0) {
+      // Arrow up
       this.setState({ cursor: cursor - 1 });
     }
-  }
+  };
 
   render() {
-    const {filteredHeroes, toHide} = this.state;
+    const { filteredHeroes, toHide, lastSearch } = this.state;
     const heroToShow = filteredHeroes.length > 0;
     const availableSpace = Object.keys(this.props.selectedHeroes).length < this.props.slots;
 
@@ -141,49 +144,50 @@ class Search extends Component {
       width: '335px',
       height: '240px',
       margin: 'auto',
-      borderBottom: '1px solid #ff81002b'
-    }
+      borderBottom: '1px solid #ff81002b',
+    };
     if (filteredHeroes.length < 8) {
       style.height = filteredHeroes.length * 30;
     }
-    if(toHide) {
-      style['display'] = 'none';
+    if (toHide) {
+      style.display = 'none';
     }
 
     return (
       <Fragment>
-        { availableSpace &&
-          <div className='search-wrapper' >
-            <form id='search' className='search' onSubmit={this.handleSubmit}>
+        {availableSpace && (
+          <div className="search-wrapper">
+            <form id="search" className="search" onSubmit={this.handleSubmit}>
               <input
-                className='search-bar'
-                type='text'
+                className="search-bar"
+                type="text"
                 ref={this.searchRef}
-                value={this.props.search}
+                value={lastSearch}
                 placeholder="Start typing a hero's name.."
                 onChange={this.props.updateSearch}
                 onFocus={this.handleFocus}
                 onBlur={this.handleBlur}
                 onKeyDown={this.handleKeyDown}
-                autoFocus={true}
               />
-              { heroToShow &&
-                <Scrollbars
-                  style={style}
-                  universal
-                  ref={ this.resultsRef }
-                  >
-                  <ul className='search-list' >
-                    {filteredHeroes.map(this.heroList)}
-                  </ul>
+              {heroToShow && (
+                <Scrollbars style={style} universal ref={this.resultsRef}>
+                  <div className="search-list">{filteredHeroes.map(this.heroList)}</div>
                 </Scrollbars>
-              }
+              )}
             </form>
           </div>
-        }
+        )}
       </Fragment>
     );
   }
 }
+
+Search.propTypes = {
+  availableHeroes: PropTypes.object.isRequired,
+  selectedHeroes: PropTypes.object.isRequired,
+  slots: PropTypes.number.isRequired,
+  updateSearch: PropTypes.func.isRequired,
+  updateSelected: PropTypes.func.isRequired,
+};
 
 export default Search;
