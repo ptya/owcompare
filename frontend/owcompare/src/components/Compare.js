cb8f2cc97a7fde0f61459b98db1de2b23b423034
ptya <petr.szab@gmail.com>
2018-06-10 14:41:23 +0200

    fixed eslint errors on Compare.js

--- frontend/owcompare/src/components/Compare.js
+++ frontend/owcompare/src/components/Compare.js
@@ -1,4 +1,6 @@
 import React, { Component, Fragment } from 'react';
+import PropTypes from 'prop-types';
+
 import heroes from '../data/heroes';
 
 import Search from './Search';
@@ -8,6 +10,10 @@ import Recommend from './Recommend';
 import { getRandomPoints } from '../helper';
 
 class Compare extends Component {
+  static Search = Search;
+  static Selection = Selection;
+  static Recommend = Recommend;
+
   state = {
     allHeroes: heroes,
     availableHeroes: heroes,
@@ -18,25 +24,20 @@ class Compare extends Component {
   };
 
   updateSearch = e => {
-    const prevSearch = { ...this.state.search };
     const nextSearch = e.target.value;
     if (nextSearch === '') {
       this.setState({ search: nextSearch });
     } else {
-      const filteredHeroes = Object.keys(this.state.availableHeroes).filter(
-        (hero) => {
-          const current = this.state.availableHeroes[hero];
-          return current.name
-            .toLowerCase()
-            .indexOf(nextSearch.toLowerCase()) !== -1 || current.id
-            .toLowerCase()
-            .indexOf(nextSearch.toLowerCase()) !== -1;
-        }
-      );
-      (filteredHeroes.length > 0) ?
-        this.setState({ search: nextSearch }) : this.setState({ prevSearch });
+      const filteredHeroes = Object.keys(this.state.availableHeroes).filter(hero => {
+        const current = this.state.availableHeroes[hero];
+        return (
+          current.name.toLowerCase().indexOf(nextSearch.toLowerCase()) !== -1 ||
+          current.id.toLowerCase().indexOf(nextSearch.toLowerCase()) !== -1
+        );
+      });
+      if (filteredHeroes.length > 0) this.setState({ search: nextSearch });
     }
-  }
+  };
 
   updateSelected = key => {
     const selectedHeroes = { ...this.state.selectedHeroes };
@@ -54,7 +55,7 @@ class Compare extends Component {
       // reset search bar
       this.setState({ search: '' });
     }
-  }
+  };
 
   removeSelected = key => {
     const selectedHeroes = { ...this.state.selectedHeroes };
@@ -65,40 +66,32 @@ class Compare extends Component {
 
     availableHeroes[key] = { ...hero };
     this.setState({ availableHeroes });
-  }
-
-  static Search = Search;
-  static Selection = Selection;
-  static Recommend = Recommend;
+  };
 
   render() {
-    const { allHeroes,
-      availableHeroes,
-      points,
-      search,
-      selectedHeroes,
-      slots } = this.state;
-    const { updateSearch,
-      updateSelected,
-      removeSelected } = this;
+    const { allHeroes, availableHeroes, points, search, selectedHeroes, slots } = this.state;
+    const { updateSearch, updateSelected, removeSelected } = this;
 
     return (
       <Fragment>
-        { this.props.children(
-            allHeroes,
-            availableHeroes,
-            points,
-            search,
-            selectedHeroes,
-            slots,
-            updateSearch,
-            updateSelected,
-            removeSelected
-          )
-        }
+        {this.props.children(
+          allHeroes,
+          availableHeroes,
+          points,
+          search,
+          selectedHeroes,
+          slots,
+          updateSearch,
+          updateSelected,
+          removeSelected
+        )}
       </Fragment>
     );
   }
 }
+
+Compare.propTypes = {
+  children: PropTypes.any.isRequired,
+};
 
 export default Compare;
