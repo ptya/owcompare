1d88ea18f97095982deece5149a6868ae840b514
ptya <petr.szab@gmail.com>
2018-06-21 22:49:07 +0200

    SearchForm using styled-components

--- frontend/owcompare/src/components/SearchForm.js
+++ frontend/owcompare/src/components/SearchForm.js
@@ -1,5 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+import { SEARCHBAR_HEIGHT, SEARCH_WIDTH } from '../constants';
+
+const StyledDiv = styled.div`
+  position: relative;
+  z-index: 999;
+`;
+
+const StyledForm = styled.form`
+  position: absolute;
+  width: 100%;
+  text-align: center;
+  top: -${SEARCHBAR_HEIGHT / 2}px;
+`;
+
+const StyledInput = styled.input`
+  box-sizing: border-box;
+  height: ${SEARCHBAR_HEIGHT}px;
+  width: ${SEARCH_WIDTH}px;
+  margin: auto;
+  padding-left: 25px;
+`;
 
 const SearchForm = props => {
   const {
@@ -13,12 +36,11 @@ const SearchForm = props => {
   } = props;
 
   return (
-    <div className="search-wrapper">
-      <form id="search" className="search" onSubmit={handleSubmit}>
-        <input
-          className="search-bar"
+    <StyledDiv>
+      <StyledForm id="search" onSubmit={handleSubmit}>
+        <StyledInput
           type="text"
-          ref={searchRef}
+          innerRef={searchRef}
           value={lastSearch}
           placeholder="Start typing a hero's name.."
           onChange={updateSearch}
@@ -27,8 +49,8 @@ const SearchForm = props => {
           onKeyDown={handleKeyDown}
         />
         {props.children}
-      </form>
-    </div>
+      </StyledForm>
+    </StyledDiv>
   );
 };
 
---
+++ frontend/owcompare/src/constants.js
@@ -0,0 +1,4 @@
+export const MAXWIDTH = '100vw';
+export const MAXHEIGHT = '100vh';
+export const SEARCHBAR_HEIGHT = 35; // px
+export const SEARCH_WIDTH = 350; // px
